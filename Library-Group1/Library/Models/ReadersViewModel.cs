f4b9cdc6b6ec1d38c4c82c9c5d59c61e022898f1
Marie <brierremarie@gmail.com>
2024-01-24 17:48:02 +0100

    readers can be created with address !

---
+++ Library-Group1/Library/Models/ReadersViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Library.Entities;
+
+namespace Library.Models
+{
+    public class ReadersViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Mail { get; set; }
+        public string PhoneNumber { get; set; }
+        public string? Appartment { get; set; }
+        public string? Street { get; set; }
+        public string? City { get; set; }
+        public string? ZipCode { get; set; }
+        public string? Country { get; set; }
+    }
+}
